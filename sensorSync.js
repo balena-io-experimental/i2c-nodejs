8cb7f163ffe8524f5ea27d1b204a98b5f278aa72
Shaun Mulligan <switchflip.shaun@gmail.com>
2016-04-29 17:43:55 +0100

    remove VCNL4000_ prefix for register names

--- sensorSync.js
+++ sensorSync.js
@@ -7,29 +7,29 @@ var conf = require('./registers.json')
 var printData = function () {
 
 	// Check product ID of sensor
-	rev = i2c1.readByteSync(conf.VCNL4000_ADDRESS, conf.VCNL4000_PRODUCTID)
+	rev = i2c1.readByteSync(conf.ADDRESS, conf.PRODUCTID)
 	if(( rev & 0xF0)  !=0x10 ){
 		console.log('no sensor found!!');
 	}
 	console.log('rev: ' + rev);
 
 	// Set the strength of IR LED
-	i2c1.writeByteSync(conf.VCNL4000_ADDRESS, conf.VCNL4000_IRLED, 5)// set to 5 * 10mA = 100mA
+	i2c1.writeByteSync(conf.ADDRESS, conf.IRLED, 5)// set to 5 * 10mA = 100mA
 
 	// Start Read for proximity data
-	i2c1.writeByteSync(conf.VCNL4000_ADDRESS, conf.VCNL4000_COMMAND, conf.VCNL4000_MEASUREPROXIMITY)
+	i2c1.writeByteSync(conf.ADDRESS, conf.COMMAND, conf.MEASUREPROXIMITY)
 	// Wait while non volatile memory busy
-	while (!((i2c1.readByteSync(conf.VCNL4000_ADDRESS, conf.VCNL4000_COMMAND) - 0x80) & conf.VCNL4000_PROXIMITYREADY)) {
+	while (!((i2c1.readByteSync(conf.ADDRESS, conf.COMMAND) - 0x80) & conf.PROXIMITYREADY)) {
 	}
-	prox = i2c1.readWordSync(conf.VCNL4000_ADDRESS,conf.VCNL4000_PROXIMITYDATA)
+	prox = i2c1.readWordSync(conf.ADDRESS,conf.PROXIMITYDATA)
 
 	// Start Read for proximity data
-	i2c1.writeByteSync(conf.VCNL4000_ADDRESS, conf.VCNL4000_COMMAND, conf.VCNL4000_MEASUREAMBIENT)
+	i2c1.writeByteSync(conf.ADDRESS, conf.COMMAND, conf.MEASUREAMBIENT)
 	// Wait while non volatile memory busy
-	while (!((i2c1.readByteSync(conf.VCNL4000_ADDRESS, conf.VCNL4000_COMMAND) - 0x80) & conf.VCNL4000_AMBIENTREADY)) {
+	while (!((i2c1.readByteSync(conf.ADDRESS, conf.COMMAND) - 0x80) & conf.AMBIENTREADY)) {
 	}
 
-	light = i2c1.readWordSync(conf.VCNL4000_ADDRESS,conf.VCNL4000_AMBIENTDATA)
+	light = i2c1.readWordSync(conf.ADDRESS,conf.AMBIENTDATA)
 
   console.log('proximity Data: ' + prox +' and light Data: ' + light);
 
