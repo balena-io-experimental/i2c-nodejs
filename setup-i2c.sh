a15255aebf8e116f55b0cf4813bf7abdb25680e0
Shaun Mulligan <switchflip.shaun@gmail.com>
2016-05-03 19:52:13 +0100

    add support for bbb and odroid-c1 in setup-i2c.sh

--- setup-i2c.sh
+++ setup-i2c.sh
@@ -8,6 +8,14 @@ I2C_BUS=0 # Default i2c bus number
 if [[ "$DEVICE_TYPE" =~ "raspberrypi" ]]; then
 	modprobe i2c-dev
 	export I2C_BUS=1
+elif [[ "$DEVICE_TYPE" = "odroid-c1" ]]; then
+	modprobe i2c-dev
+	modprobe aml_i2c
+	export I2C_BUS=1
+elif [[ "$DEVICE_TYPE" = "beaglebone" ]]; then
+	# load cape-universaln if you need i2c-1
+	#echo cape-universaln > /sys/devices/platform/bone_capemgr/slots
+	export I2C_BUS=2
 elif [[ "$DEVICE_TYPE" = "artik5" ]]; then
 	# Currently not working :/
 	export I2C_BUS=9
